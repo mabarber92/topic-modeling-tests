757b9846ad8e0a8e656cabb258266ebdb522eb71
mabarber92 <mathew.barber@aku.edu>
2023-03-07 18:05:38 +0000

    undertook analysis of topics

--- BERTopic/tasks/modelSearches.py
+++ BERTopic/tasks/modelSearches.py
@@ -12,7 +12,7 @@ def modelSearches(searchCsv, outCsv, summaryCsv, vizOut, treeOut, saveModel = No
     if not existingModel:
         
         topicModel, summaryDf = csvToBERTopic(searchDf, outCsv, inputType='df', returnModel=True, sentenceField="phrase", returnSummary=10, returnSummaryCsv = summaryCsv,
-        colSummary=[{'col1': 'ms', 'col2': 'uri'}], embeddingModel="CAMeL-Lab/bert-base-arabic-camelbert-ca", seed=seed)
+        colSummary=[{'col1': 'ms', 'col2': 'uri'}], seed=seed)
     else:
         topicModel = BERTopic.load(existingModel)
     
@@ -38,18 +38,23 @@ if __name__ == "__main__":
     inputFile = "../../data/inputs/searchResults/largerWindow/results.csv"
     LargeOutputFolder = "../../data/outputs/searchModelling/"
     outputFolder = "./output/"
-    outCsv = LargeOutputFolder + "results-camelbert-seed100-run1.csv"
-    outSummary = outputFolder + "results-camelbert-seed100-summary-run1.csv"
-    modelOut = outputFolder + "results-camelbert-seed100.model"
-    vizOut = outputFolder + "results-camelbert-seed100-viz-whier-run1.html"
-    treeOut = outputFolder + "results-camelbert-seed100-tree-whier-run1.txt"
+    outCsv = LargeOutputFolder + "results-arabert-seed100-run1.csv"
+    outSummary = outputFolder + "results-arabert-seed100-summary-run1.csv"
+    modelOut = outputFolder + "results-arabert-seed100.model"
+    vizOut = outputFolder + "results-arabert-seed100-viz-whier-run1.html"
+    treeOut = outputFolder + "results-arabert-seed100-tree-whier-run1.txt"
 
-
+    outCsv2 = LargeOutputFolder + "results-arabert-seed100-run2.csv"
+    outSummary2 = outputFolder + "results-arabert-seed100-summary-run2.csv"
+    modelOut2 = outputFolder + "results-arabert-seed100.model"
+    vizOut2 = outputFolder + "results-arabert-seed100-viz-whier-run2.html"
+    treeOut2 = outputFolder + "results-arabert-seed100-tree-whier-run2.txt"
     # cutInput = outputFolder + "cutInput.csv"
     # dfCut = pd.read_csv(inputFile).loc[0:5000]
     # dfCut.to_csv(cutInput, index = False, encoding = 'utf-8-sig')
 
     modelSearches(inputFile, outCsv, outSummary, vizOut, treeOut, modelOut, seed=100)
+    modelSearches(inputFile, outCsv2, outSummary2, vizOut2, treeOut2, modelOut, seed=100)
 
 
 
